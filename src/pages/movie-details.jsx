d2d1054de4913cafc4d1ced2bcfa08c7ee1e90f3
atharv0811 <karnekaratharv12@gmail.com>
2025-02-10 18:29:50 +0530

    some layout changes

--- src/pages/movie-details.jsx
+++ src/pages/movie-details.jsx
@@ -70,22 +70,24 @@ const MovieDetails = () => {
             <div className="max-w-[90%] md:max-w-[1180px] mx-auto my-6 ms-4">
                 <h2 className="text-2xl font-bold mb-4">Cast</h2>
 
-                <div className="flex items-center gap-4 overflow-y-auto w-full">
-                    {movieCasts.map((cast) => {
-                        return (
-                            <div key={cast.id} className="flex flex-col items-center">
-                                <img
-                                    src={imgUrl + cast.profile_path}
-                                    alt=""
-                                    className="w-20 md:w-28 h-20 md:h-28 rounded-full object-cover"
-                                />
-                                <p className="text-sm md:text-base text-center">{cast.name}</p>
-                                <p className="text-gray-500 text-sm">
-                                    {cast.known_for_department === "Acting" && "Actor"}
-                                </p>
-                            </div>
-                        );
-                    })}
+                <div className="overflow-x-auto no-scrollbar">
+                    <div className="w-[30rem] md:w-full flex items-start gap-4">
+                        {movieCasts.map((cast) => {
+                            return (
+                                <div key={cast.id} className="flex flex-col items-center">
+                                    <img
+                                        src={imgUrl + cast.profile_path}
+                                        alt=""
+                                        className="w-20 md:w-28 h-20 md:h-28 rounded-full object-cover"
+                                    />
+                                    <p className="text-sm md:text-base text-center">{cast.name}</p>
+                                    <p className="text-gray-500 text-sm">
+                                        {cast.known_for_department === "Acting" && "Actor"}
+                                    </p>
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
             </div>
 
@@ -94,20 +96,22 @@ const MovieDetails = () => {
             <div className="max-w-[90%] md:max-w-[1180px] mx-auto my-6 ms-4">
                 <h2 className="text-2xl font-bold mb-4">Crew</h2>
 
-                <div className="flex items-center gap-4 overflow-y-auto w-full">
-                    {movieCrew.map((crew) => {
-                        return (
-                            <div key={crew.id} className="flex flex-col items-center">
-                                <img
-                                    src={imgUrl + crew.profile_path}
-                                    alt=""
-                                    className="w-20 md:w-28 h-20 md:h-28 rounded-full object-cover"
-                                />
-                                <p className="text-sm md:text-base text-center">{crew.name}</p>
-                                <p className="text-gray-500 text-sm">{crew.job}</p>
-                            </div>
-                        );
-                    })}
+                <div className="overflow-x-auto no-scrollbar">
+                    <div className="w-[30rem] md:w-full flex items-start gap-4">
+                        {movieCrew.map((crew) => {
+                            return (
+                                <div key={crew.id} className="flex flex-col items-center">
+                                    <img
+                                        src={imgUrl + crew.profile_path}
+                                        alt=""
+                                        className="w-20 md:w-28 h-20 md:h-28 rounded-full object-cover"
+                                    />
+                                    <p className="text-sm md:text-base text-center">{crew.name}</p>
+                                    <p className="text-gray-500 text-sm">{crew.job}</p>
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
             </div>
         </div>
